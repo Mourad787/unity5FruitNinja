ab6b9428190d2dcbc522c2998442191a100601ed
mourad767 <327665@student.mboutrecht.nl>
2020-01-31 22:58:22 +0100

    no message

---
+++ Assets/script/Target.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+
+    private float miniSpeed = 14;
+
+    private float maxSpeed = 17;
+
+    private float maxTorque = 10;
+
+    private float xRange = 4;
+
+    private float ySpawnpos = -6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        
+        // deze code werkt niet wegens Random.range ik heb uitgezocht maar kom er niet uit.
+        targetRb.AddTorque(RandomTorque(), RandomTorque(),RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(miniSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+       return new Vector3(Random.Range(-xRange, xRange), ySpawnpos);
+    }
+}
